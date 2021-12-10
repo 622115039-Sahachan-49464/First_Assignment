196b5525c16ce2d655b1c3ec10946e89ed888fd4
oat431 <oat431@gmail.com>
2021-12-10 14:53:40 +0700

    [🖊 add] :sidebar

--- lib/component/daily_art_sidebar.dart
+++ lib/component/daily_art_sidebar.dart
@@ -3,8 +3,38 @@ import 'package:flutter/material.dart';
 class DailyArtSideBar extends StatelessWidget {
   const DailyArtSideBar({Key? key}) : super(key: key);
 
+  Widget dailyTile(Icon icon,String text,BuildContext context){
+    return ListTile(
+      leading: icon,
+      title: Text(text),
+      onTap: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.purple,
+            ),
+            child: Text('The Profile Pic',style: TextStyle(color: Colors.white),),
+          ),
+          dailyTile(const Icon(Icons.auto_awesome_mosaic_outlined),"Today", context),
+          dailyTile(const Icon(Icons.save),"Archive", context),
+          dailyTile(const Icon(Icons.star),"Get Lucky", context),
+          dailyTile(const Icon(Icons.money_outlined),"Donation list", context),
+          dailyTile(const Icon(Icons.feedback),"Feed back", context),
+          dailyTile(const Icon(Icons.share),"Share", context),
+          dailyTile(const Icon(Icons.settings),"Setting", context),
+          dailyTile(const Icon(Icons.info_outline),"About", context),
+        ],
+      ),
+    );
   }
 }
