fdb793eb5107f8fe49e25ee02292afe497de0de7
oat431 <oat431@gmail.com>
2021-12-10 14:11:07 +0700

    [✨ add] : appbar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:se494_first_assignment/component/daily_art_appbar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      appBar: DailyArtAppBar(),
+    );
   }
 }
